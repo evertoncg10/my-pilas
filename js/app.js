5e79f7c41430c3c0e793a9b7d4e35b08e9c2beda
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-10-04 13:11:29 -0300

    chaveando o menu.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var alternador = document.getElementById("botao-chaveador");
+
+alternador.onclick = function () {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("menu--exibindo");
+};
